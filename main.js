a0b6ba600678f8664a64b08560a7b0ff93afbba3
VINICIUS ALVES DOS SANTOS <22282103@CFP127ALUNO.LOCAL>
2023-02-16 11:27:08 -0300

    em andamento

--- main.js
+++ main.js
@@ -12,28 +12,45 @@ const adicionarCard = (aluno, nota) => {
     const container = document.getElementById('container')
     const novaDiv = document.createElement('div')
     novaDiv.classList.add('aluno')
-    novaDiv.innerHTML = `<h2 class="aluno__title">${aluno}</h2> <p class= "nota__title">${nota}</p>`
+    novaDiv.innerHTML = `<h2 class="aluno__title">${aluno}</h2> <p class = "nota__title">${nota}</p>`
     container.appendChild(novaDiv)
 
-    if (nota > 5) {
+    if ((nota > 5)) {
         novaDiv.classList.add('aprovado')
         novaDiv.classList.remove('reprovado')
     } else {
         novaDiv.classList.add('reprovado')
         novaDiv.classList.remove('aprovado')
-
     }
 }
 
 
 const handleClick = () => {
-        const nomeAluno = prompt("Digite o nome do aluno")
-        const notaAluno = prompt("Digite a nota do aluno")
-        adicionarCard(nomeAluno, notaAluno)
+    const nomeAluno = prompt("Digite o nome do aluno")
+    const notaAluno = prompt("Digite a nota do aluno")
 
+    if (notaAluno > 10) {
+        alert("ERRO")
+    } else if (isNaN(notaAluno)) {
+        alert("ERRO")
 
+    } else if (notaAluno < 0) {
+        alert("ERRO")
+    } else if (notaAluno = "") {
+        alert("ERRO")
 
-        adicionar.addEventListener('click', handleClick)
+    } else if (nomeAluno = "") {
+        alert("ERRO")
     }
-    // SOLID - Responsividade unica
-    // Funções puras
+
+
+
+
+    adicionarCard(nomeAluno, notaAluno)
+
+}
+
+adicionar.addEventListener('click', handleClick)
+
+// SOLID - Responsividade unica
+// Funções puras
